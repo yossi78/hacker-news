2c63e15d4e0d4d3c2b62c17f68ec538ce543b5a7
yossi78 <yossi78@gmail.com>
2024-12-24 12:07:49 +0200

    Add BL for post

---
+++ src/main/java/com/example/hacker_news/api/PostController.java
@@ -0,0 +1,23 @@
+package com.example.hacker_news.api;
+import com.example.hacker_news.model.Post;
+import com.example.hacker_news.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/hacker_news/model/Post.java
@@ -0,0 +1,34 @@
+package com.example.hacker_news.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
